d1b5b2c95e2f8ac6a81404b7c27b79e9f8f2c309
Dave B <imageinteractive@outlook.com>
2024-09-29 20:52:20 -0400

    Removed use of bootstrap that existed in the default Razor Pages project template.

--- JsDotNetInterop/Services/FormaterService.cs
+++ JsDotNetInterop/Services/FormaterService.cs
@@ -4,7 +4,7 @@
     // https://stackoverflow.com/questions/70046997/how-would-you-call-methods-from-a-different-class-in-a-razor-class-file-in-blazo/70048391#70048391
     internal class FormaterService
     {
-        public string? FormattedSerialNum { get; private set; }
+        public string? FormattedSerialNum { get; set; }
 
         public string? FormatSerial(int? id)
         {
